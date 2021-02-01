92627690e29b0555865596d95181855d4dd4aafa
J.Laro <joselarojr@gmail.com>
2021-02-01 16:30:26 -0500

    import/export - template, beginning to refactor code

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Veggie Burger ');
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Cheese Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Royale Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Double Cheese', true);
+INSERT INTO burgers (name) VALUES ('Mushroom');
